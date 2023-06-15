ee168034ff612bdc8334181f2f023e49833d34c3
flavio <flavioborinjunior@gmail.com>
2023-06-14 21:49:45 -0300

    vou me matei

--- lambda2.hs
+++ lambda2.hs
@@ -3,12 +3,13 @@ data Term =
     Var Int
     | Abs Term
     | App Term Term
+        deriving Show
 
-instance Show Term where
-  show t = case t of
-    Var x     -> show x
-    Abs t1    -> "λ" ++ show t1
-    App t1 t2 -> "(" ++ show t1 ++ " " ++ show t2 ++ ")"
+-- instance Show Term where
+--   show t = case t of
+--     Var x     -> show x
+--     Abs t1    -> "/" ++ show t1
+--     App t1 t2 -> "(" ++ show t1 ++ " " ++ show t2 ++ ")"
 
 lFalse = Abs (
             Abs 
@@ -51,6 +52,12 @@ lOne = Abs (
         )
     )
 
+lTwo = Abs (
+        Abs(
+            App (Var 1) (App (Var 1) (Var 0)) 
+        )
+    )
+
 lSum = Abs (
         Abs (
             Abs(
@@ -73,95 +80,49 @@ shift d term = walk 0 term
                 Abs t1 -> Abs (walk (c+1) t1)
                 App t1 t2 -> App (walk c t1) (walk c t2)
 
--- subst o j termo em t por s
+-- subst o j termo de t por s
 subst :: Int -> Term -> Term -> Term
 subst j s t = walk 0 t
     where walk c t = case t of
-                Var x -> if x == j
-                         then shift j s 
+                Var x -> if x == j + c
+                         then shift (c + 1) s 
                          else Var x
-                Abs t1 -> Abs (walk (c+1) t1)
+                Abs t1 -> Abs (walk (c + 1) t1)
                 App t1 t2 -> App (walk c t1) (walk c t2)
 
 --substitui o termo s em t e deshifta de volta
 substTop :: Term -> Term -> Term
 substTop s t = shift (-1) (subst 0 (shift 1 s) t)
 
--- evalI :: Term -> Term
--- evalI term = case term of 
---     App (Abs t12) v2@(Abs _) -> evalI $ substTop v2 t12
---     App v1@(Abs _) t2 -> let t2' = evalI t2
---                          in evalI (App v1 t2')
---     App t1 t2 -> let t1' = evalI t1
---                  in evalI (App t1' t2)
---     Abs t1 -> Abs $ evalI t1 
---     _ -> term
 
-evalI :: Term -> IO Term
-evalI t@(Abs t1) = do
+isVal :: Term -> Bool
+isVal (Abs _) = False
+isVal _ = True
+
+evalPrint :: Term -> IO Term
+evalPrint t@(App (Abs a) b) | isVal b = do
     print t
-    print 1
-    next <- evalI t1
-    return (Abs next)
-evalI t@(App (Abs t1) t2@(Abs _)) = do
+    print "App isval b"
+    let y = substTop b a
+    print $ "substutindo b em a = " ++ show y
+    evalPrint y
+evalPrint t@(App a b) | isVal a = do
     print t
-    print 2
-    evalI $ substTop t2 t1
-evalI t@(App t1@(Abs _) t2) = do
+    print "App isval a"
+    b' <- evalPrint b
+    evalPrint $ App a b'
+evalPrint t@(App a b) = do 
+    print t 
+    print "App generico"
+    a' <- evalPrint a
+    return $ App a' b
+evalPrint t = do
     print t
-    print 3
-    t2' <- evalI t2
-    evalI (App t1 t2')
-evalI t@(App t1 t2) = do 
-    print t
-    print 4
-    t1' <- evalI t1
-    evalI (App t1' t2)
-evalI t = do
-    print t
-    print 0
-    return t
+    print "Fim"
+    return t 
 
- 
+-- main :: IO()
+-- main = print $ subst 0 lNot (Abs (App (Var 0) lTrue))
 
--- main :: IO ()
--- main = do
---     coiso <- evalI ( App (App lSum lOne) lOne )
---     print coiso
-
-
-
--- tmMap :: (Int -> Int -> IO Term) -> Int -> Term -> IO Term
--- tmMap f c t = walk c t
---     where
---         walk c t@(Var x) = do 
---                                 print t
---                                 f c x
---         walk c t@(Abs t1) = do
---                                 print t
---                                 aux <- walk (c+1) t 
---                                 return (Abs aux)
---         walk c t@(App t1 t2) = do
---                                     print t
---                                     aux1 <- walk c t1
---                                     aux2 <- walk c t2
---                                     return (App aux1 aux2)
-
--- shifti :: Int -> Int -> Term -> IO Term
--- shifti d c t = tmMap (\c x -> if x >= c then return (Var (x + d)) else return(Var x)) c t
-
--- shift :: Term -> Int -> IO Term
--- shift t d = shifti d 0 t
-
--- substi :: Term -> Int -> Term -> IO Term
--- substi s j t = tmMap (\j x -> if x == j then shift s j
---                                         else return (Var x)) j t
-
--- subst :: Term -> Term -> IO Term
--- subst s t = substi s 0 t
-
--- substnip :: Term -> Term -> IO Term
--- substnip s t = do 
---     aux <- shift s 1
---     aux2 <- subst aux t
---     shift aux2  (-1) 
+main :: IO Term 
+main = evalPrint $ App (Abs (Abs (App (Var 0) (Var 1)))) (Var 90)
