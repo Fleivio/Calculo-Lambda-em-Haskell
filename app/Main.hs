9205bb76d778004916971378b571fd2ad7e54365
flavio <flavioborinjunior@gmail.com>
2023-06-19 09:24:07 -0300

    Primeira tentativa com funcoes de 1 argumento

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,23 @@ module Main (main) where
 
 import Expr
 
+-- test :: Expr
+-- test = Op Mult (Op Sum (Number 3) (Def "func")) (Number 10)
+
+-- vart :: VarTable
+-- vart = [("func", Let "x" (Number 3) (Op Pow (Number 2) (Def "x")))]
+
+-- ltest :: Expr
+-- ltest = Lamb ["x"] (Op Sum (Def "x") (Number 1))
+
+-- ltestApply ::  Expr
+-- ltestApply = Apply ltest (Number 3)
+
 test :: Expr
-test = Op Mult (Op Sum (Number 3) (Def "func")) (Number 10)
+test = Apply ( Apply (Def "myFunc") (Number 3) ) (Number 4)
 
 vart :: VarTable
-vart = [("func", Let "x" (Number 3) (Op Pow (Number 2) (Def "x")))]
+vart = [("myFunc", Lamb ["x", "y"] (Op Sum (Def "x") (Def "y")))]
 
 main :: IO ()
 main = print $ evalExpr test vart
--- src/Expr.hs
+++ src/Expr.hs
@@ -21,63 +21,65 @@ data Expr =
 
 type VarTable = [(VarName, Expr)]
 
--- data EvaluatedExpr = Expr Expr | Error String deriving (Show, Eq)
+data Evaluated a = Valid a | Error String deriving (Show, Eq)
 
--- instance Functor EvaluatedExpr where
---   fmap f (Expr e) = Expr $ f e
---   fmap _ (Error s) = Error s
+instance Functor Evaluated where
+  fmap f (Valid e) = Valid $ f e
+  fmap _ (Error s) = Error s
 
--- instance Applicative EvaluatedExpr where
---   pure = Expr
---   (Expr f) <*> (Expr e) = Expr $ f e
---   (Error s) <*> (Error t) = Error (s ++ " && " ++ t)
---   (Error s) <*> _ = Error s
---   _ <*> (Error s) = Error s  
+instance Applicative Evaluated where
+  pure = Valid
+  (Valid f) <*> (Valid e) = Valid $ f e
+  (Error s) <*> (Error t) = Error (s ++ " && " ++ t)
+  (Error s) <*> _ = Error s
+  _ <*> (Error s) = Error s  
 
--- instance Monad EvaluatedExpr where
---   return = pure
---   (Expr e) >>= f = f e
---   (Error s) >>= _ = Error s
+instance Monad Evaluated where
+  return = pure
+  (Valid e) >>= f = f e
+  (Error s) >>= _ = Error s
 
-exprToTerm :: Expr -> VarTable -> Maybe Expr
-exprToTerm (Term t)         _  = return $ Term (evalTerm t)
-exprToTerm (Number n)       _  = return $ Term (evalTerm ( numToTerm n ))
-exprToTerm (Op op a b)      vt = evalMathOp op a b vt
-exprToTerm (Def name)       vt = (lookupVar name vt) >>= \e -> exprToTerm e vt
-exprToTerm (Let name e1 e2) vt = exprToTerm e2 $ (name, e1):vt
+instance MonadFail Evaluated where
+  fail = Error 
 
-evalMathOp :: MathOp -> Expr -> Expr -> VarTable -> Maybe Expr
-evalMathOp op a b vt = res >>= (return . Term)
-  where res = case op of 
+evalExpr :: Expr -> VarTable -> Evaluated Expr
+evalExpr (Term t)                        _  = return $ Term (evalTerm t)
+evalExpr (Number n)                      _  = return $ Term (evalTerm ( numToTerm n ))
+evalExpr (Op op a b)                     vt = evalMathOp op a b vt
+evalExpr (Def name)                      vt = (lookupVar name vt) >>= \e -> evalExpr e vt
+evalExpr (Let name e1 e2)                vt = evalExpr e2 $ (name, e1):vt
+evalExpr (Apply (Def name) e2)           vt = do
+                                                e1 <- (lookupVar name vt)
+                                                evalExpr (Apply e1 e2) vt
+evalExpr (Apply (Lamb (a : args) e1) e2) vt = do
+                                                body <- (lambApply e1 a e2 vt)
+                                                case args of
+                                                  [] -> return body
+                                                  _  -> return $ Lamb args body
+evalExpr (Apply f1 e2)                   vt = evalExpr f1 vt >>= \f -> evalExpr (Apply f e2) vt
+evalExpr a _ = Error $ "Invalid Expression: " ++ show a
+
+lambApply :: Expr -> VarName -> Expr -> VarTable -> Evaluated Expr
+lambApply e1 name e2 vt = do
+  e2' <- evalExpr e2 vt
+  e1' <- evalExpr e1 ((name, e2'):vt)
+  return e1'
+
+evalMathOp :: MathOp -> Expr -> Expr -> VarTable -> Evaluated Expr
+evalMathOp op a b vt = case op of
                 Sum  -> evalThenApply a b sumToTerm vt
                 Sub  -> evalThenApply a b subToTerm vt
                 Mult -> evalThenApply a b multToTerm vt
                 Pow  -> evalThenApply a b powToTerm vt
 
-lookupVar :: VarName -> VarTable -> Maybe Expr
-lookupVar name vt = fmap snd $ find (\(n, _) -> n == name) vt
+lookupVar :: VarName -> VarTable -> Evaluated Expr
+lookupVar name vt = case value of
+    Nothing -> Error $ "Var Not Found " ++ name
+    Just e  -> return e
+  where value = fmap snd $ find (\(n, _) -> n == name) vt
 
--- substVarExpr :: Expr -> VarName -> Expr -> Expr 
--- substVarExpr (Def name) _ n e =
---   | n == name = e
---   | otherwise = Def name
--- substVarExpr (Op f a b) = Op f (substVarExpr a) (substVarExpr b)
--- substVarExpr (Let name e1 e2) = Let name (substVarExpr e1) (substVarExpr e2)
--- substVarExpr (Lamb args e) = Lamb args (substVarExpr e)
--- substVarExpr total _ _ = total
-
-
-evalThenApply :: Expr -> Expr -> (Term -> Term -> Term) -> VarTable -> Maybe Term
+evalThenApply :: Expr -> Expr -> (Term -> Term -> Term) -> VarTable -> Evaluated Expr
 evalThenApply a b f vt = do
-    (Term a') <- exprToTerm a vt
-    (Term b') <- exprToTerm b vt
-    return $ evalTerm $ f a' b'
-
-evalExpr :: Expr -> VarTable -> Maybe Expr
-evalExpr expr vt =  evaltd >>= \e -> case e of
-                                      Term t -> case termToNum t of
-                                                  Just n -> return $ Number n
-                                                  Nothing -> return e
-                                      _ -> return e
-  where
-    evaltd = exprToTerm expr vt
+    (Term a') <- evalExpr a vt
+    (Term b') <- evalExpr b vt
+    return . Term $ evalTerm $ f a' b'
