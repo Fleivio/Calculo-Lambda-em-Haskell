105e8b23e35107fd3d3cb1dcb342818b5048ae78
flavio <flavioborinjunior@gmail.com>
2023-06-23 18:51:09 -0300

    explorada consequencia de numeros serem funcoes

--- program.f
+++ program.f
@@ -5,5 +5,7 @@ fn fib = /x. if (x == 0) || (x == 1)
 fn fact = /x. if (x == 0) 
               then 1 
               else x * (fact (x - 1));
-              
-fact 4
+
+fn veztres = /x. 2*x;
+        
+(veztres (veztres 2)) == ((2 veztres) 2)
